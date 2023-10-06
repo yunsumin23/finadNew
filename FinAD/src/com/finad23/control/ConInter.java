9b131914dcaa1913ae0c489dfaa93618152c4509
Administrator <Administrator@User -2023RKYYE>
2023-10-06 10:59:46 +0900

    마지막 테스트

--- FinAD/src/com/finad23/control/ConInter.java
+++ FinAD/src/com/finad23/control/ConInter.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 // �������̽� ����� �а���
 // ���� �������̽��� ������ ������ �ϸ� ���� �������̽��� �����ϸ� ��.
 // 윤수민 비응으으으으신
+//홍카콜라
 public interface ConInter {
 	String FinAD(HttpServletRequest rq, HttpServletResponse rs) throws Exception;
 }
