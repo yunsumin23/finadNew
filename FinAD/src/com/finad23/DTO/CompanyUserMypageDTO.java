1ce5d4ecff85e62eef60ec0618c3dd846fbe7655
Administrator <Administrator@User -2023RKYYE>
2023-11-21 18:21:13 +0900

    윤수민 기업 마이페이지 DTO

---
+++ FinAD/src/com/finad23/DTO/CompanyUserMypageDTO.java
@@ -0,0 +1,64 @@
+package com.finad23.DTO;
+
+public class CompanyUserMypageDTO {
+
+	private String companyId;
+	private String image;
+	private String companyLink;
+	private String previousAdvertising1;
+	private String previousAdvertising2;
+	private String previousAdvertising3;
+	private String introduction;
+	private String company_mypageNum;
+	
+	public String getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getCompanyLink() {
+		return companyLink;
+	}
+	public void setCompanyLink(String companyLink) {
+		this.companyLink = companyLink;
+	}
+	public String getPreviousAdvertising1() {
+		return previousAdvertising1;
+	}
+	public void setPreviousAdvertising1(String previousAdvertising1) {
+		this.previousAdvertising1 = previousAdvertising1;
+	}
+	public String getPreviousAdvertising2() {
+		return previousAdvertising2;
+	}
+	public void setPreviousAdvertising2(String previousAdvertising2) {
+		this.previousAdvertising2 = previousAdvertising2;
+	}
+	public String getPreviousAdvertising3() {
+		return previousAdvertising3;
+	}
+	public void setPreviousAdvertising3(String previousAdvertising3) {
+		this.previousAdvertising3 = previousAdvertising3;
+	}
+	public String getIntroduction() {
+		return introduction;
+	}
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+	public String getCompany_mypageNum() {
+		return company_mypageNum;
+	}
+	public void setCompany_mypageNum(String company_mypageNum) {
+		this.company_mypageNum = company_mypageNum;
+	}
+	
+//	companyId, image, companyLink, previousAdvertising1, previousAdvertising2, previousAdvertising3, previousAdvertising4, introduction, company_mypageNum
+}
